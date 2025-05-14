45064645356ff53900a634200698d23dce4335fb
Aureo-Bueno <aureoalexandre@outlook.com>
2025-05-13 21:10:24 -0300

    fix: add default route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ export default function AppRoutes() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
